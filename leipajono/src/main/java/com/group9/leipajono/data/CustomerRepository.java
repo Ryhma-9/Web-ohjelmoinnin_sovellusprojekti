bba9b572bbe553431894048b29739b3d3baee9df
Tero Vähäsarja <90072945+terbaali@users.noreply.github.com>
2022-03-25 13:15:36 +0200

    Delete leipajono directory

--- leipajono/src/main/java/com/group9/leipajono/data/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group9.leipajono.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-    
-}
