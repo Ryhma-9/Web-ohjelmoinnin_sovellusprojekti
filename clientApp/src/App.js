ef3fc1e6bb9e61a463e5cb516211f89f261583cc
Foxglade <kapu.kettu@gmail.com>
2022-04-18 09:48:48 +0300

    patch

--- clientApp/src/App.js
+++ clientApp/src/App.js
@@ -11,7 +11,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleInfo, faInfo } from '@fortawesome/free-solid-svg-icons'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import RestaurantProfile from './components/RestaurantProfile';
-
+import MenuEdit from './MenuEdit';
 
 function App() {
   // Jotain toiminnallisuutta kaupungin valinnan hallintaan
@@ -40,6 +40,7 @@ function App() {
           <Route path="/shoppingcart" element={<ShoppingCart  /* Shoppings={ (shoppingCartItems) => { setShoppingCartItems(shoppingCartItems)}} *//>} />
           <Route path="/payment" element={ <Payment  />}/>
           <Route path="/restaurantprofile" element={ <RestaurantProfile/>}/>
+          <Route path='/menuedit' element={<MenuEdit/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- clientApp/src/components/Customerprofile.js
+++ clientApp/src/components/Customerprofile.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react'
 import './LogInBox.css';
-import jwt_decode from "jwt-decode";
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Constants from './Constants.json';
 
 export default function CustomerProfile(props) {
 const [userName, setUserName] = useState("");
 const [role, setRole] = useState("");
 
-function handleToken(){
-  var token = props.jwt;
-  var decoded = jwt_decode(token);
-  console.log(decoded);
-  setUserName(decoded.sub);
-  setRole(decoded.role);
+const handleToken = async () => {
+  const instance = axios.create({
+  baseURL: Constants.API_ADDRESS,
+  timeout: 1000,
+  headers: {'Authorization': 'Bearer '+props.jwt}
+  })
+  const result = await instance.get('/private');
+  setUserName(result.data.userName);
+  setRole(result.data.role);
 }
+handleToken();
 
 return (props.trigger) ? (
   <div className="popup">
@@ -34,12 +38,6 @@ return (props.trigger) ? (
         </div>
         <div>
         <button className="logout-btn" onClick={() => { props.setJwtToken(null); props.setCounter(); props.setTrigger(false); }}>Logout</button>
-        </div>
-        <div>
-        <button className="check-btn" onClick={() => { handleToken() } }>Check User Information</button>
-        </div>
-        <div>
-          <Link to="/restaurantprofile"><button className="edit-btn" >Edit profile</button></Link>
         </div>
         {props.children}
       </div> 
