b9151d11ad4a73ef6ac6f7e30b2c6ac23e64327b
Foxglade <kapu.kettu@gmail.com>
2022-04-18 17:43:19 +0300

    Update Restaurantprofile.js

--- clientApp/src/components/Restaurantprofile.js
+++ clientApp/src/components/Restaurantprofile.js
@@ -8,9 +8,50 @@ import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
 import './RestaurantProfile.css'
 import './Profiili.css'
 import jwt_decode from "jwt-decode";
+import Constants from './Constants.json'
 
 export default function RestaurantProfile() {
+
+  const [restaurantName, setrestaurantName] = useState();
+  const [restaurantAddress, setrestaurantAddress] = useState();
+  const [restaurantUserName, setrestaurantUserName] = useState();
+  const [restaurantEmail, setrestaurantEmail] = useState();
+  const [restaurantPhoneNumber, setrestaurantPhoneNumber] = useState();
+  const [restaurantStyle, setrestaurantStyle] = useState();
+  const [restaurantPriceRange, setrestaurantPriceRange] = useState();
+  const [restaurantCity, setrestaurantCity] = useState();
+  const [openinghours, setopeninghours] = useState();
   
+  
+  const restaurantNamechange = (event) => {
+    setrestaurantName(event.target.value);
+  }
+  const restaurantAddresschange = (event) => {
+    setrestaurantAddress(event.target.value);
+  }
+  const restaurantCitychange = (event) => {
+    setrestaurantCity(event.target.value);
+  }
+  const restaurantEmailchange = (event) => {
+    setrestaurantEmail(event.target.value);
+  }
+  const restaurantPhoneNumberchange = (event) => {
+    setrestaurantPhoneNumber(event.target.value);
+  }
+  const restaurantPriceRangechange = (event) => {
+    setrestaurantPriceRange(event.target.value);
+  }
+  const openinghourschange = (event) => {
+    setopeninghours(event.target.value);
+  }
+  const restaurantStylechange = (event) => {
+    setrestaurantStyle(event.target.value);
+  }
+
+  const results = axios.get(Constants.API_ADDRESS ,{
+
+  });
+
   const userLoggedIn = 'peksi';
   const CreateRestaurantTypeOptions = () => {
     const options = [
@@ -63,16 +104,14 @@ export default function RestaurantProfile() {
     <div className='forminner'>
     <h2>Edit restaurant</h2>
   <form onSubmit={ onRestaurantAddition }>
-    <div className=""><div className=''>Restaurant name: </div> <input type="text" name="restaurantName"/></div>
-    <div className=""><div className=''>Restaurant address: </div> <input type="text" name="restaurantAddress"/></div>
-    <div className=""><div className=''>City: </div> <input type="text" name="restaurantCity"/></div>
-    <div className=""><div className=''>Email: </div> <input type="text" name="restaurantEmail"/></div>
-    <div className=""><div className=''>Phonenumber: </div> <input type="text" name="restaurantPhoneNumber"/></div>
-    <div className=""><div className=''>Price range: </div> <input type="text" name="restaurantPriceRange"/></div>
-    <div className=""><div className=''>Opening hours: </div> <input type="text" name="openinghours"/></div>
-    <div className="">
-      <div className=''>Restaurant style: </div>
-      <select className="" name="restaurantStyle" >
+    <div className=""><div className=''>Restaurant name: </div> <input type="text" name="restaurantName" value={restaurantName} onChange={ restaurantNamechange } /></div>
+    <div className=""><div className=''>Restaurant address: </div> <input type="text" name="restaurantAddress" value={restaurantAddress} onChange={ restaurantAddresschange}/></div>
+    <div className=""><div className=''>City: </div> <input type="text" name="restaurantCity" value={restaurantCity} onChange={ restaurantCitychange}/></div>
+    <div className=""><div className=''>Email: </div> <input type="text" name="restaurantEmail" value={restaurantEmail} onChange={ restaurantEmailchange}/></div>
+    <div className=""><div className=''>Phonenumber: </div> <input type="text" name="restaurantPhoneNumber" value={restaurantPhoneNumber} onChange={ restaurantPhoneNumberchange}/></div>
+    <div className=""><div className=''>Price range: </div> <input type="text" name="restaurantPriceRange" value={restaurantPriceRange} onChange={ restaurantPriceRangechange}/></div>
+    <div className=""><div className=''>Opening hours: </div> <input type="text" name="openinghours" value={openinghours} onChange={ openinghourschange}/></div>
+    <div className=""><div className=''>Restaurant style: </div> <select className="" name="restaurantStyle" value={restaurantStyle} onChange={restaurantStylechange}> 
         <CreateRestaurantTypeOptions /> 
       </select>
     </div>
